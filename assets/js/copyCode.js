4098388028251e5ad5fac929cfcc0f5bae296c61
nebulaOW <77023146+nebulaOW@users.noreply.github.com>
2021-01-16 18:04:37 -0600

    Update copyCode.js

--- assets/js/copyCode.js
+++ assets/js/copyCode.js
@@ -1,5 +1,20 @@
 const copyCode = (clickEvent) => {
-  // The magic happens here
+  const copyCodeButton = clickEvent.target;
+  const tempTextArea = document.createElement('textarea');
+  tempTextArea.textContent = copyCodeButton.getAttribute('data-code');
+  document.body.appendChild(tempTextArea);
+
+  const selection = document.getSelection();
+  selection.removeAllRanges();
+  tempTextArea.select();
+  document.execCommand('copy');
+  selection.removeAllRanges();
+  document.body.removeChild(tempTextArea);
+
+  copyCodeButton.classList.add('copied');
+  setTimeout(() => {
+    copyCodeButton.classList.remove('copied');
+}, 2000);
 };
 
 document.querySelectorAll('.copy-code-button').forEach((copyCodeButton) => {
